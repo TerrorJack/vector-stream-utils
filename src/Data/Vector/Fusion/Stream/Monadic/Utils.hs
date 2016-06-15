8dd86a9e2fd8847e661b3c08391970b64a803132
Shao Cheng <astrohavoc@gmail.com>
2016-06-16 03:15:10 +0800

    Add getRandomStreamR.

--- src/Data/Vector/Fusion/Stream/Monadic/Utils.hs
+++ src/Data/Vector/Fusion/Stream/Monadic/Utils.hs
@@ -24,9 +24,14 @@ constStream m = Stream (const m) ()
 hGetChunkStream :: (IOData a, MonadIO m) => Handle -> Stream m a
 hGetChunkStream h = constStream $ do
     chunk <- hGetChunk h
-    if onull chunk then pure Done else pure $ Yield chunk ()
+    pure $ if onull chunk then Done else Yield chunk ()
 
 getRandomStream :: (PrimMonad m, Variate a) => Gen (PrimState m) -> Stream m a
 getRandomStream g = constStream $ do
     x <- uniform g
+    pure $ Yield x ()
+
+getRandomStreamR :: (PrimMonad m, Variate a) => Gen (PrimState m) -> (a, a) -> Stream m a
+getRandomStreamR g r = constStream $ do
+    x <- uniformR r g
     pure $ Yield x ()
